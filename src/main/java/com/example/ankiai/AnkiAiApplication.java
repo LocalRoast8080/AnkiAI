524623f733ab1d74dd2e3f1accb93b1582a1b951
Chris Patton <localroast8080@gmail.com>
2025-03-07 13:53:40 -0500

    main clean up, added swagger

--- src/main/java/com/example/ankiai/AnkiAiApplication.java
+++ src/main/java/com/example/ankiai/AnkiAiApplication.java
@@ -1,11 +1,9 @@
 package com.example.ankiai;
 
 import com.example.ankiai.configuration.XAiConfigProperties;
-import com.example.ankiai.services.NoteCardProcessorService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @EnableConfigurationProperties(XAiConfigProperties.class)
